bd37fdf602ff5757795a5bd610593eea91d403bd
Mehdi Afrasiabi <mr.abbann1976@gmail.com>
2026-07-07 19:38:37 +0330

    updaate seeder

--- database/seeders/CcGradesTableSeeder.php
+++ database/seeders/CcGradesTableSeeder.php
@@ -14,133 +14,133 @@ class CcGradesTableSeeder extends Seeder
      */
     public function run()
     {
-
+        
 
         \DB::table('cc_grades')->delete();
-
+        
         \DB::table('cc_grades')->insert(array (
-            0 =>
-                array (
-                    'id' => 1,
-                    'education_level_id' => 1,
-                    'cc_field_id' => 1,
-                    'name' => 'پایه دوازدهم (ریاضی و فیزیک)',
-                    'grade_number' => 12,
-                    'order' => 0,
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:08:27',
-                    'updated_at' => '2026-02-07 14:08:27',
-                ),
-            1 =>
-                array (
-                    'id' => 2,
-                    'education_level_id' => 1,
-                    'cc_field_id' => 2,
-                    'name' => 'پایه دوازدهم (علوم تجربی)',
-                    'grade_number' => 12,
-                    'order' => 1,
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:08:53',
-                    'updated_at' => '2026-02-07 14:08:53',
-                ),
-            2 =>
-                array (
-                    'id' => 3,
-                    'education_level_id' => 1,
-                    'cc_field_id' => 3,
-                    'name' => 'پایه دوازدهم (علوم انسانی)',
-                    'grade_number' => 12,
-                    'order' => 2,
-                    'is_active' => 0,
-                    'created_at' => '2026-02-07 14:09:20',
-                    'updated_at' => '2026-02-08 18:54:26',
-                ),
-            3 =>
-                array (
-                    'id' => 5,
-                    'education_level_id' => 1,
-                    'cc_field_id' => 1,
-                    'name' => 'پایه یازدهم (ریاضی و فیزیک)',
-                    'grade_number' => 11,
-                    'order' => 4,
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:10:28',
-                    'updated_at' => '2026-02-07 14:10:28',
-                ),
-            4 =>
-                array (
-                    'id' => 6,
-                    'education_level_id' => 1,
-                    'cc_field_id' => 2,
-                    'name' => 'پایه یازدهم (علوم تجربی)',
-                    'grade_number' => 11,
-                    'order' => 5,
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:10:44',
-                    'updated_at' => '2026-02-07 14:10:44',
-                ),
-            5 =>
-                array (
-                    'id' => 7,
-                    'education_level_id' => 1,
-                    'cc_field_id' => 3,
-                    'name' => 'پایه یازدهم (علوم انسانی)',
-                    'grade_number' => 11,
-                    'order' => 6,
-                    'is_active' => 0,
-                    'created_at' => '2026-02-07 14:11:03',
-                    'updated_at' => '2026-02-08 18:55:22',
-                ),
-            6 =>
-                array (
-                    'id' => 9,
-                    'education_level_id' => 1,
-                    'cc_field_id' => 1,
-                    'name' => 'پایه دهم (ریاضی و فیزیک)',
-                    'grade_number' => 10,
-                    'order' => 8,
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:12:20',
-                    'updated_at' => '2026-02-07 14:12:20',
-                ),
-            7 =>
-                array (
-                    'id' => 10,
-                    'education_level_id' => 1,
-                    'cc_field_id' => 2,
-                    'name' => 'پایه دهم (علوم تجربی)',
-                    'grade_number' => 10,
-                    'order' => 9,
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:12:46',
-                    'updated_at' => '2026-02-07 14:12:46',
-                ),
-            8 =>
-                array (
-                    'id' => 11,
-                    'education_level_id' => 1,
-                    'cc_field_id' => 3,
-                    'name' => 'پایه دهم (علوم انسانی)',
-                    'grade_number' => 10,
-                    'order' => 10,
-                    'is_active' => 0,
-                    'created_at' => '2026-02-07 14:13:16',
-                    'updated_at' => '2026-02-08 18:56:45',
-                ),
-            9 =>
-                array (
-                    'id' => 12,
-                    'education_level_id' => 2,
-                    'cc_field_id' => NULL,
-                    'name' => 'پایه نهم ',
-                    'grade_number' => 9,
-                    'order' => 0,
-                    'is_active' => 1,
-                    'created_at' => '2026-02-16 14:50:38',
-                    'updated_at' => '2026-02-16 14:50:38',
-                ),
+            0 => 
+            array (
+                'id' => 1,
+                'education_level_id' => 1,
+                'cc_field_id' => 1,
+            'name' => 'پایه دوازدهم (ریاضی و فیزیک)',
+                'grade_number' => 12,
+                'order' => 0,
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:08:27',
+                'updated_at' => '2026-02-07 14:08:27',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'education_level_id' => 1,
+                'cc_field_id' => 2,
+            'name' => 'پایه دوازدهم (علوم تجربی)',
+                'grade_number' => 12,
+                'order' => 1,
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:08:53',
+                'updated_at' => '2026-02-07 14:08:53',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'education_level_id' => 1,
+                'cc_field_id' => 3,
+            'name' => 'پایه دوازدهم (علوم انسانی)',
+                'grade_number' => 12,
+                'order' => 2,
+                'is_active' => 0,
+                'created_at' => '2026-02-07 14:09:20',
+                'updated_at' => '2026-02-08 18:54:26',
+            ),
+            3 => 
+            array (
+                'id' => 5,
+                'education_level_id' => 1,
+                'cc_field_id' => 1,
+            'name' => 'پایه یازدهم (ریاضی و فیزیک)',
+                'grade_number' => 11,
+                'order' => 4,
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:10:28',
+                'updated_at' => '2026-02-07 14:10:28',
+            ),
+            4 => 
+            array (
+                'id' => 6,
+                'education_level_id' => 1,
+                'cc_field_id' => 2,
+            'name' => 'پایه یازدهم (علوم تجربی)',
+                'grade_number' => 11,
+                'order' => 5,
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:10:44',
+                'updated_at' => '2026-02-07 14:10:44',
+            ),
+            5 => 
+            array (
+                'id' => 7,
+                'education_level_id' => 1,
+                'cc_field_id' => 3,
+            'name' => 'پایه یازدهم (علوم انسانی)',
+                'grade_number' => 11,
+                'order' => 6,
+                'is_active' => 0,
+                'created_at' => '2026-02-07 14:11:03',
+                'updated_at' => '2026-02-08 18:55:22',
+            ),
+            6 => 
+            array (
+                'id' => 9,
+                'education_level_id' => 1,
+                'cc_field_id' => 1,
+            'name' => 'پایه دهم (ریاضی و فیزیک)',
+                'grade_number' => 10,
+                'order' => 8,
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:12:20',
+                'updated_at' => '2026-02-07 14:12:20',
+            ),
+            7 => 
+            array (
+                'id' => 10,
+                'education_level_id' => 1,
+                'cc_field_id' => 2,
+            'name' => 'پایه دهم (علوم تجربی)',
+                'grade_number' => 10,
+                'order' => 9,
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:12:46',
+                'updated_at' => '2026-02-07 14:12:46',
+            ),
+            8 => 
+            array (
+                'id' => 11,
+                'education_level_id' => 1,
+                'cc_field_id' => 3,
+            'name' => 'پایه دهم (علوم انسانی)',
+                'grade_number' => 10,
+                'order' => 10,
+                'is_active' => 0,
+                'created_at' => '2026-02-07 14:13:16',
+                'updated_at' => '2026-02-08 18:56:45',
+            ),
+            9 => 
+            array (
+                'id' => 12,
+                'education_level_id' => 2,
+                'cc_field_id' => NULL,
+                'name' => 'پایه نهم ',
+                'grade_number' => 9,
+                'order' => 0,
+                'is_active' => 1,
+                'created_at' => '2026-02-16 14:50:38',
+                'updated_at' => '2026-02-16 14:50:38',
+            ),
         ));
-
-
+        
+        
     }
 }
--- database/seeders/SubjectsTableSeeder.php
+++ database/seeders/SubjectsTableSeeder.php
@@ -14,193 +14,193 @@ class SubjectsTableSeeder extends Seeder
      */
     public function run()
     {
-
+        
 
         \DB::table('subjects')->delete();
-
+        
         \DB::table('subjects')->insert(array (
-            0 =>
-                array (
-                    'id' => 1,
-                    'name' => 'ریاضی',
-                    'slug' => 'mathematics',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            1 =>
-                array (
-                    'id' => 2,
-                    'name' => 'فیزیک',
-                    'slug' => 'physics',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            2 =>
-                array (
-                    'id' => 3,
-                    'name' => 'شیمی',
-                    'slug' => 'chemistry',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            3 =>
-                array (
-                    'id' => 4,
-                    'name' => 'زیست‌شناسی',
-                    'slug' => 'biology',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            4 =>
-                array (
-                    'id' => 5,
-                    'name' => 'ادبیات فارسی',
-                    'slug' => 'persian-literature',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            5 =>
-                array (
-                    'id' => 6,
-                    'name' => 'عربی',
-                    'slug' => 'arabic',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            6 =>
-                array (
-                    'id' => 7,
-                    'name' => 'زبان انگلیسی',
-                    'slug' => 'english',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            7 =>
-                array (
-                    'id' => 8,
-                    'name' => 'دین و زندگی',
-                    'slug' => 'religion',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            8 =>
-                array (
-                    'id' => 9,
-                    'name' => 'تاریخ',
-                    'slug' => 'history',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            9 =>
-                array (
-                    'id' => 10,
-                    'name' => 'جغرافیا',
-                    'slug' => 'geography',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            10 =>
-                array (
-                    'id' => 11,
-                    'name' => 'اقتصاد',
-                    'slug' => 'economics',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            11 =>
-                array (
-                    'id' => 12,
-                    'name' => 'جامعه‌شناسی',
-                    'slug' => 'sociology',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            12 =>
-                array (
-                    'id' => 13,
-                    'name' => 'فلسفه و منطق',
-                    'slug' => 'philosophy-logic',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            13 =>
-                array (
-                    'id' => 14,
-                    'name' => 'روانشناسی',
-                    'slug' => 'psychology',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            14 =>
-                array (
-                    'id' => 15,
-                    'name' => 'هندسه',
-                    'slug' => 'geometry',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            15 =>
-                array (
-                    'id' => 16,
-                    'name' => 'آمار و احتمال',
-                    'slug' => 'statistics',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            16 =>
-                array (
-                    'id' => 17,
-                    'name' => 'حسابان',
-                    'slug' => 'calculus',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            17 =>
-                array (
-                    'id' => 18,
-                    'name' => 'گسسته',
-                    'slug' => 'discrete-math',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            18 =>
-                array (
-                    'id' => 19,
-                    'name' => 'زمین‌شناسی',
-                    'slug' => 'geology',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
-            19 =>
-                array (
-                    'id' => 20,
-                    'name' => 'علوم اجتماعی',
-                    'slug' => 'social-sciences',
-                    'is_active' => 1,
-                    'created_at' => '2026-02-07 14:04:06',
-                    'updated_at' => '2026-02-07 14:04:06',
-                ),
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'ریاضی',
+                'slug' => 'mathematics',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'فیزیک',
+                'slug' => 'physics',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => 'شیمی',
+                'slug' => 'chemistry',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'name' => 'زیست‌شناسی',
+                'slug' => 'biology',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'name' => 'ادبیات فارسی',
+                'slug' => 'persian-literature',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'name' => 'عربی',
+                'slug' => 'arabic',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'name' => 'زبان انگلیسی',
+                'slug' => 'english',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'name' => 'دین و زندگی',
+                'slug' => 'religion',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            8 => 
+            array (
+                'id' => 9,
+                'name' => 'تاریخ',
+                'slug' => 'history',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            9 => 
+            array (
+                'id' => 10,
+                'name' => 'جغرافیا',
+                'slug' => 'geography',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            10 => 
+            array (
+                'id' => 11,
+                'name' => 'اقتصاد',
+                'slug' => 'economics',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            11 => 
+            array (
+                'id' => 12,
+                'name' => 'جامعه‌شناسی',
+                'slug' => 'sociology',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            12 => 
+            array (
+                'id' => 13,
+                'name' => 'فلسفه و منطق',
+                'slug' => 'philosophy-logic',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            13 => 
+            array (
+                'id' => 14,
+                'name' => 'روانشناسی',
+                'slug' => 'psychology',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            14 => 
+            array (
+                'id' => 15,
+                'name' => 'هندسه',
+                'slug' => 'geometry',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            15 => 
+            array (
+                'id' => 16,
+                'name' => 'آمار و احتمال',
+                'slug' => 'statistics',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            16 => 
+            array (
+                'id' => 17,
+                'name' => 'حسابان',
+                'slug' => 'calculus',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            17 => 
+            array (
+                'id' => 18,
+                'name' => 'گسسته',
+                'slug' => 'discrete-math',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            18 => 
+            array (
+                'id' => 19,
+                'name' => 'زمین‌شناسی',
+                'slug' => 'geology',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
+            19 => 
+            array (
+                'id' => 20,
+                'name' => 'علوم اجتماعی',
+                'slug' => 'social-sciences',
+                'is_active' => 1,
+                'created_at' => '2026-02-07 14:04:06',
+                'updated_at' => '2026-02-07 14:04:06',
+            ),
         ));
-
-
+        
+        
     }
 }
